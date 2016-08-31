b05ab00213de0b3ef09ffffc8e434082006462f9
Ayris <jelte.kroes@ayris.nl>
2016-08-31 17:32:14 +0000

    Modified devise for stripe

--- saasapp10/db/schema.rb
+++ saasapp10/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831165228) do
+ActiveRecord::Schema.define(version: 20160831173020) do
 
   create_table "plans", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160831165228) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
